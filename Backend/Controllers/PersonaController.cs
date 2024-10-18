06c3b3817501f96bd5af31f74f3195a86d415e88
AndreaN <nm0753032023@unab.edu.sv>
2024-10-18 13:43:14 -0600

    Guia9: Filtrar la información

--- Backend/Controllers/PersonaController.cs
+++ Backend/Controllers/PersonaController.cs
@@ -8,7 +8,14 @@ namespace Backend.Controllers
     public class PersonaController : Controller
     {
         [HttpGet("all")]
+        public List<PersonaDatos> Get() => Repository.persona;
 
-        public List<PersonaDatos> GetPersonaDatos() => Repository.persona;
+
+        [HttpGet("{id}")]
+        public PersonaDatos GetPersonaDatos(int id) => Repository.persona.FirstOrDefault(p => p.id == id);
+
+
+        [HttpGet("search/{search}")]
+        public List<PersonaDatos> Get(string search) => Repository.persona.Where(p => p.name.ToUpper().Contains(search.ToUpper())).ToList();
     }
 }
