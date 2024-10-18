cac54bfee6cae27bd8a47bf078f379147e42894d
AndreaN <nm0753032023@unab.edu.sv>
2024-10-18 00:27:55 -0600

    Guia 8: Respuestas de dato especifico

---
+++ Backend/Controllers/PersonaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class PersonaController : Controller
+    {
+        [HttpGet("all")]
+
+        public List<PersonaDatos> GetPersonaDatos() => Repository.persona;
+    }
+}
---
+++ Backend/Controllers/Repository.cs
@@ -0,0 +1,36 @@
+﻿namespace Backend.Controllers
+{
+    public class Repository
+    {
+        public static List<PersonaDatos> persona = new List<PersonaDatos>
+        {
+            new PersonaDatos()
+            {
+                id = 1,
+                age = new DateTime(2005,06,28),
+                name = "Andrea Alexandra Nuñez"
+            },
+
+            new PersonaDatos()
+            {
+                id = 2,
+                age = new DateTime(2005,06, 29),
+                name = "Andrea Alexandra"
+            },
+
+            new PersonaDatos()
+            {
+                id = 3,
+                age = new DateTime(2005,06, 30),
+                name = "Andrea"
+            }
+        };
+    }
+}
+
+public class PersonaDatos
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public DateTime age { get; set; }
+}
